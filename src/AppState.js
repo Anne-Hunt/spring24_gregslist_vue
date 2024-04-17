279afe6cd4851ad3828ace58d9fe4111b50c1f2b
Anne Hunt <acathunt@gmail.com>
2024-04-17 13:19:56 -0600

    AppState Updated

--- src/AppState.js
+++ src/AppState.js
@@ -8,5 +8,11 @@ export const AppState = reactive({
   account: null,
 
   /** @type {import('./models/Car.js').Car[]} */
-  cars: []
+  cars: [],
+
+  /** @type {import('./models/House.js').House[]} */
+  houses: [],
+
+  /** @type {import('./models/Job.js').Job[]} */
+  jobs: []
 })
