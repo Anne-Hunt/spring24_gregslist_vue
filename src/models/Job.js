04d4ec1e397ba60645620bc64ee7b00403864818
Anne Hunt <acathunt@gmail.com>
2024-04-17 13:17:17 -0600

    models created

---
+++ src/models/Job.js
@@ -0,0 +1,16 @@
+
+
+export class Job {
+    constructor(data) {
+        this.id = data.id
+        this.company = data.company
+        this.jobTitle = data.jobTitle
+        this.hours = data.hours
+        this.rate = data.rate
+        this.description = data.description
+        this.createdAt = new Date(data.createdAt)
+        this.updatedAt = data.updatedAt
+        this.creatorId = data.creatorId
+        this.creator = data.creator
+    }
+}
